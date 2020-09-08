617d6226ec3a8d890facbbbad932fc979d358cb6
Tony Melek <tonymelek.au@gmail.com>
2020-09-08 11:12:40 +1000

    Adding some comments

--- app.js
+++ app.js
@@ -1,22 +1,37 @@
+//initialising myMoment to be set for time and date now
 let myMoment = moment();
+//initialising an empty object for data storage 
 let StorageObj = {}
+//run the main function
 manipulate()
 
-
+//navigate dates backward
 $(".fa-arrow-circle-right").click(() => {
     myMoment = myMoment.add(1, 'day')
     manipulate()
 })
+//navigate dates forward
 $(".fa-arrow-circle-left").click(() => {
     myMoment = myMoment.subtract(1, 'day')
     manipulate()
 })
-
+function display_date() {
+    if (window.innerWidth > 580) {
+        $("#currentDay").text(myMoment.format('dddd , Do [of] MMMM, YYYY	'))
+    }
+    else {
+        //Display shorter date format on smaller screens
+        $("#currentDay").text(myMoment.format('ddd, DD/MM/YYYY'))
+    }
+}
+window.onresize = display_date //for responsive experience the function runs when the window is resized
+//main function
 function manipulate() {
-
-    $("#currentDay").text(myMoment.format('dddd , Do [of] MMMM, YYYY	'))
+    //Display selected Date on screen 
+    display_date();
+    // Set timeNow to be 24hour format integer value of the current hour
     const timeNow = parseInt(moment().format('HH'))
-
+    //read data in localStorage and put it in the Storage object created @ initialisation
     if (localStorage.getItem(myMoment.format('DD/MM/YY')) !== null) {
         StorageObj = {
             [myMoment.format('DD/MM/YY')]: JSON.parse(localStorage.getItem(myMoment.format('DD/MM/YY')))
@@ -26,9 +41,13 @@ function manipulate() {
             [myMoment.format('DD/MM/YY')]: {}
         }
     }
-    $(".container").empty()
-    for (i = 9; i < 18; i++) {
+
+    $(".container").empty() //empty the container before the new build
+
+    for (i = 9; i < 18; i++) { //Loop to generate rows and blocks for every hour from 9:00 am to 5:00 pm
+        //Set time of Block every time the loop is executed
         let timeOfBlock = parseInt(myMoment.startOf('day').add(i, 'hour').format('HH').slice(0, 2))
+        //Access DoM using JQuery to create elements and set attributes
         let newRow = $("<div class='row'></div")
         let newHour = $("<div class='col-2 col-lg-1  hour '></div>")
         let newDesc = $("<div class='col-8 col-lg-10 '></div>")
@@ -40,11 +59,16 @@ function manipulate() {
         newRow.append(newHour)
         newRow.append(newDesc)
         newRow.append(newSave)
+        //Add created content to the empty container
         $(".container").append(newRow)
+        //check Storage Object for this specifix date and time, if it has data add it to the text of the corresponding hour
         if (!jQuery.isEmptyObject(StorageObj[myMoment.format('DD/MM/YY')]) && StorageObj[myMoment.format('DD/MM/YY')][timeOfBlock] !== undefined) {
             $("#text" + timeOfBlock).val(StorageObj[myMoment.format('DD/MM/YY')][timeOfBlock])
         }
+        //Add hour text with HH:mm AM/PM format
         newHour.text((myMoment.startOf('day').add(i, 'hour').format('LT')))
+        //compare current data and hour to all blocks and add calsses to get them coloured accordingly
+        //if they are in past or present or future
         if (myMoment.startOf('Day').valueOf() === moment().startOf('Day').valueOf()) {
             if (timeOfBlock > timeNow) {
                 newDesc.addClass("future")
@@ -60,29 +84,17 @@ function manipulate() {
             newDesc.addClass("past")
         }
     }
-    //Save the typed info by clicking "Save Button" 
-    $(".saveBtn").click(function () {
-        StorageObj[myMoment.format('DD/MM/YY')][this.id] = $("#text" + this.id).val()
-        localStorage.setItem([myMoment.format('DD/MM/YY')], JSON.stringify(StorageObj[myMoment.format('DD/MM/YY')]))
-    })
-    //Save the typed info by clicking "Enter"  
-    $(".description").on('keyup', function (e) {
-        if (e.key === "Enter") {
-            StorageObj[myMoment.format('DD/MM/YY')][this.id.slice(4)] = $("#" + this.id).val()
-            localStorage.setItem([myMoment.format('DD/MM/YY')], JSON.stringify(StorageObj[myMoment.format('DD/MM/YY')]))
-        }
-    })
+
 }
-
-
-// document.addEventListener('click', function (e) {
-//   if (e.target.className.includes('saveBtn')) {
-//     console.log("you clicked me")
-//     StorageObj[myMoment.format('DD/MM/YY')][e.target.id] = $("#text" + e.target.id).val()
-//     localStorage.setItem([myMoment.format('DD/MM/YY')], JSON.stringify(StorageObj[myMoment.format('DD/MM/YY')]))
-//   } else if (e.target.parentElement.className.includes('saveBtn')) {
-//     console.log("you clicked me")
-//     StorageObj[myMoment.format('DD/MM/YY')][e.target.parentElement.id] = $("#text" + e.target.parentElement.id).val()
-//     localStorage.setItem([myMoment.format('DD/MM/YY')], JSON.stringify(StorageObj[myMoment.format('DD/MM/YY')]))
-//   }
-// })
+// Save to local Storage on Click
+$(document).on('click', '.saveBtn', function () {
+    StorageObj[myMoment.format('DD/MM/YY')][this.id] = $("#text" + this.id).val()
+    localStorage.setItem([myMoment.format('DD/MM/YY')], JSON.stringify(StorageObj[myMoment.format('DD/MM/YY')]))
+})
+// Save to local Storage on hitting 'Enter' key
+$(document).on('keyup', '.description', function (e) {
+    if (e.key === "Enter") {
+        StorageObj[myMoment.format('DD/MM/YY')][this.id.slice(4)] = $("#" + this.id).val()
+        localStorage.setItem([myMoment.format('DD/MM/YY')], JSON.stringify(StorageObj[myMoment.format('DD/MM/YY')]))
+    }
+})
